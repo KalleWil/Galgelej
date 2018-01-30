6401fceb0003f7dcff25d8fe3e67a187b489cdac
jullle3 <jullefis@gmail.com>
2018-01-30 17:05:48 +0100

    Add files via upload

---
+++ Galgeleg/src/galgeleg/GalgeInterface.java
@@ -0,0 +1,15 @@
+package galgeleg; // skal stå øverst
+
+import java.rmi.*;
+
+/**
+ *
+ * @author Julian
+ */
+public interface GalgeInterface extends Remote
+{
+    void nulstil() throws java.rmi.RemoteException;
+    void logStatus() throws java.rmi.RemoteException;
+    void gætBogstav(String bogstav)  throws java.rmi.RemoteException;
+    boolean erSpilletSlut()  throws java.rmi.RemoteException;
+}
