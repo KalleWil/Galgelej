fb607319dd8a4771ed47a5ee8fa45cc7eaf7cf37
julle <julle@DESKTOP-41C6OL2.eduroam.wireless.dtu.dk>
2018-02-07 13:01:50 +0100

    .

--- src/galgeleg/GalgeInterface.java
+++ src/galgeleg/GalgeInterface.java
@@ -1,17 +1,20 @@
 package galgeleg; // skal stå øverst
 
 import java.rmi.*;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
 
 /**
  *
  * @author Julian
  */
+@WebService // til soap
 public interface GalgeInterface extends Remote
 {
-    void nulstil()              throws java.rmi.RemoteException;
-    void logStatus()            throws java.rmi.RemoteException;
-    void gætBogstav(String bogstav)  throws java.rmi.RemoteException;
-    boolean erSpilletSlut()     throws java.rmi.RemoteException;
-    String outputTilKlient()    throws java.rmi.RemoteException;
+    @WebMethod void nulstil()              throws java.rmi.RemoteException;
+    @WebMethod void logStatus()            throws java.rmi.RemoteException;
+    @WebMethod void gætBogstav(String bogstav)  throws java.rmi.RemoteException;
+    @WebMethod boolean erSpilletSlut()     throws java.rmi.RemoteException;
+    @WebMethod String outputTilKlient()    throws java.rmi.RemoteException;
   
 }
