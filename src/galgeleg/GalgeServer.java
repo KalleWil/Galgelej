fd6c4342c1079524bc2ce7da061f1d64c3cdd005
julle <julle@DESKTOP-41C6OL2.eduroam.wireless.dtu.dk>
2018-02-07 13:02:33 +0100

    Opsat til at køre på jacobs server

--- src/galgeleg/GalgeServer.java
+++ src/galgeleg/GalgeServer.java
@@ -3,15 +3,19 @@ package galgeleg;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import javax.xml.ws.Endpoint;
 
 public class GalgeServer 
 {
     public static void main(String[] args) throws RemoteException, MalformedURLException 
     {
-        java.rmi.registry.LocateRegistry.createRegistry(1099);
+        java.rmi.registry.LocateRegistry.createRegistry(1450);//(1099);
         
         GalgeInterface g = new Galgelogik();
-        Naming.rebind("rmi://localhost/GalgeServer", g);
+        // Naming.rebind("rmi://ubuntu4.saluton.dk:9919/GalgeServer", g);    // rmi
+        //Endpoint.publish("http://ubuntu4.saluton.dk:9919/kontotjeneste", g); // soap
+	Endpoint.publish("http://ubuntu4.saluton.dk:9919/GalgeServer", g); // soap. Serveren udgiver sig som "GalgeServer"
+		
         System.out.println("Galgeserver opstartet");
         
     }
