d8dfd4538b3ab81f5924c2fe0ed15aa1386f26b0
julle <julle@DESKTOP-41C6OL2.eduroam.wireless.dtu.dk>
2018-02-07 16:42:40 +0100

    Fixet fejl, som gjorde at serveren genbrugte ord

--- src/galgeleg/GalgeServer.java
+++ src/galgeleg/GalgeServer.java
@@ -7,14 +7,14 @@ import javax.xml.ws.Endpoint;
 
 public class GalgeServer 
 {
-    public static void main(String[] args) throws RemoteException, MalformedURLException 
+    public static void main(String[] args) throws RemoteException, MalformedURLException, Exception 
     {
         java.rmi.registry.LocateRegistry.createRegistry(1450);//(1099);
         
         GalgeInterface g = new Galgelogik();
-        // Naming.rebind("rmi://ubuntu4.saluton.dk:9919/GalgeServer", g);    // rmi
-        //Endpoint.publish("http://ubuntu4.saluton.dk:9919/kontotjeneste", g); // soap
-	Endpoint.publish("http://ubuntu4.saluton.dk:9919/GalgeServer", g); // soap. Serveren udgiver sig som "GalgeServer"
+         //Naming.rebind("rmi://localhost:9919/GalgeServer", g);    // rmi - localhost
+        Endpoint.publish("http://localhost:9919/GalgeServer", g); // soap - localhost
+	//Endpoint.publish("http://ubuntu4.saluton.dk:9919/GalgeServer", g); // soap. Serveren udgiver sig som "GalgeServer"
 		
         System.out.println("Galgeserver opstartet");
         
